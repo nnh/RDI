74fa7eceea57a0949b2abf302d1b2ff172fc72d8
Toshiki Saito <tosh@jerusalem.local>
2017-10-25 15:38:38 +0900

    select records with max therapy_cd

--- program/RDI_pgm.R
+++ program/RDI_pgm.R
@@ -1,9 +1,14 @@
+library(survival)
+library(risksetROC)
 # Read input data
-setwd("./input")
-ads1 <- read.csv('170427_ADS1.csv', as.is = T, fileEncoding = 'UTF-8-BOM')
-ads2 <- read.csv('170427_ADS2.csv', as.is = T, fileEncoding = 'UTF-8-BOM')
-setwd("..")
+ads1 <- read.csv('input/170427_ADS1.csv', as.is = T, fileEncoding = 'UTF-8-BOM')
+ads2 <- read.csv('input/170427_ADS2.csv', as.is = T, fileEncoding = 'UTF-8-BOM')
 
+MAX_T <- tapply(ads2$REAL_THERAPY_CD, ads2$ID, max)
+max_therapy <- data.frame(MAX_T)
+max_therapy$ID <- row.names(MAX_T)
+ads3 <- merge(ads2, max_therapy, by = "ID")
+ads4 <- ads3[ads3$REAL_THERAPY_CD == ads3$MAX_T, ]
 
 survival.time=ads2$EFS_DAY
 survival.status=ads2$EFS_FLG
