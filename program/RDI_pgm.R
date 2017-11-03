96090ed93de4791a1ac246b33798415007104095
Toshiki Saito <tosh@jerusalem.local>
2017-11-03 21:44:20 +0900

    add comments

--- program/RDI_pgm.R
+++ program/RDI_pgm.R
@@ -4,13 +4,15 @@ library(risksetROC)
 ads1 <- read.csv('input/170427_ADS1.csv', as.is = T, fileEncoding = 'UTF-8-BOM')
 ads2 <- read.csv('input/170427_ADS2.csv', as.is = T, fileEncoding = 'UTF-8-BOM')
 
-MAX_T <- tapply(ads2$REAL_THERAPY_CD, ads2$ID, max)
-max_therapy <- data.frame(MAX_T)
-max_therapy$ID <- row.names(MAX_T)
+# Make ADS
+MAX_T <- tapply(ads2$REAL_THERAPY_CD, ads2$ID, max)   # extract max therapy code for each patient ID
+max_therapy <- data.frame(MAX_T)   # make dataframe only with max therapy code
+max_therapy$ID <- row.names(MAX_T)   # make ID column
 ads3 <- merge(ads2, max_therapy, by = "ID")
-ads4 <- ads3[ads3$REAL_THERAPY_CD == ads3$MAX_T, ]
-ads5 <- ads4[!is.na(ads4$EFS_DAY) & !is.na(ads4$EFS_FLG), ]
+ads4 <- ads3[ads3$REAL_THERAPY_CD == ads3$MAX_T, ]   # extract rows only with max therapy code
+ads5 <- ads4[!is.na(ads4$EFS_DAY) & !is.na(ads4$EFS_FLG), ]   # extract rows without NA in EFS_DAY and EFS_FLG
 
+# ROC analysis
 survival.time <- ads5$EFS_DAY
 survival.status <- ads5$EFS_FLG
 
@@ -22,6 +24,7 @@ AUC <- NULL
 out <- CoxWeights(marker=eta, Stime=survival.time, status=survival.status, predict.time=10)
 AUC <- out$AUC
 
+# REFERNCE
 # library(survival)
 # library(risksetROC)
 # library(MASS)
