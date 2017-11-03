1dd8eee03e4d04c457417352c34e6adc0fc9ae39
Toshiki Saito <tosh@jerusalem.local>
2017-11-04 08:14:12 +0900

    derive disease risk

--- program/RDI_pgm.R
+++ program/RDI_pgm.R
@@ -12,6 +12,18 @@ ads3 <- merge(ads2, max_therapy, by = "ID")
 ads4 <- ads3[ads3$REAL_THERAPY_CD == ads3$MAX_T, ]   # extract rows only with max therapy code
 ads5 <- ads4[!is.na(ads4$EFS_DAY) & !is.na(ads4$EFS_FLG), ]   # extract rows without NA in EFS_DAY and EFS_FLG
 
+ads5$CYTO_T821[is.na(ads5$CYTO_T821)] <- 0
+ads5$CYTO_INV16[is.na(ads5$CYTO_INV16)] <- 0
+ads5$FLT3_ITD1[is.na(ads5$FLT3_ITD1)] <- 0
+ads5$CYTO_7[is.na(ads5$CYTO_7)] <- 0
+ads5$CYTO_5Q[is.na(ads5$CYTO_5Q)] <- 0
+ads5$CYTO_T1621[is.na(ads5$CYTO_T1621)] <- 0
+ads5$CYTO_PH1[is.na(ads5$CYTO_PH1)] <- 0
+ads5$FLT3_ITD1[is.na(ads5$FLT3_ITD1)] <- 0
+
+ads5$disease_risk <- ifelse((ads5$CYTO_T821 == 1 | ads5$CYTO_INV16 == 1) & (ads5$FLT3_ITD1 == 1), 1,
+                     ifelse((ads5$CYTO_7 == 1 | ads5$CYTO_5Q == 1 | ads5$CYTO_T1621 == 1 | ads5$CYTO_PH1 == 1 | ads5$FLT3_ITD1 == 1), 3, 2))
+
 # ROC analysis
 survival.time <- ads5$EFS_DAY
 survival.status <- ads5$EFS_FLG
