6c506eba17d8f9ffec50e9b001f30812fb480110
Akiko Saito <akiko.saito@nnh.go.jp>
2017-11-03 22:16:11 +0900

    change predict.time and delete comment

--- program/RDI_pgm.R
+++ program/RDI_pgm.R
@@ -10,39 +10,16 @@ max_therapy <- data.frame(MAX_T)   # make dataframe only with max therapy code
 max_therapy$ID <- row.names(MAX_T)   # make ID column
 ads3 <- merge(ads2, max_therapy, by = "ID")
 ads4 <- ads3[ads3$REAL_THERAPY_CD == ads3$MAX_T, ]   # extract rows only with max therapy code
-ads5 <- ads4[!is.na(ads4$EFS_DAY) & !is.na(ads4$EFS_FLG), ]   # extract rows without NA in EFS_DAY and EFS_FLG
+ads5 <- ads4[!is.na(ads4$EFS_DAY) & !is.na(ads4$EFS_FLG), ]   # extract rows w/o NA in EFS_DAY and EFS_FLG
 
 # ROC analysis
 survival.time <- ads5$EFS_DAY
 survival.status <- ads5$EFS_FLG
 
-fit0 <- coxph(Surv(EFS_DAY, EFS_FLG) ~ ARDI, data=ads5, na.action=na.omit)
+fit0 <- coxph(Surv(EFS_DAY, EFS_FLG) ~ ARDI1, data=ads5, na.action=na.omit)
 eta <- fit0$linear.predictors
-ROC.CC10 <- risksetROC(Stime=survival.time, status=survival.status, marker=eta, predict.time=10, method="Cox",
-                       main="ROC Curve", lty=2, col="red")
+ROC.CC10 <- risksetROC(Stime=survival.time, status=survival.status, marker=eta, predict.time=180,
+                       method="Cox", main="ROC Curve", lty=2, col="red")
 AUC <- NULL
-out <- CoxWeights(marker=eta, Stime=survival.time, status=survival.status, predict.time=10)
+out <- CoxWeights(marker=eta, Stime=survival.time, status=survival.status, predict.time=180)
 AUC <- out$AUC
-
-# REFERNCE
-# library(survival)
-# library(risksetROC)
-# library(MASS)
-# data(VA)
-# survival.time <- VA$stime
-# survival.status <- VA$status
-# score <- VA$Karn
-# cell.type <- factor(VA$cell)
-# tx <- as.integer(VA$treat == 1)
-# age <- VA$age
-# survival.status[survival.time > 500 ] <- 0
-# survival.time[survival.time > 500 ] <- 500
-# fit0 <- coxph(Surv(survival.time, survival.status) ~ score + cell.type + tx + age, na.action=na.omit)
-# summary(fit0)
-# eta <- fit0$linear.predictor
-# ROC.CC30 <- risksetROC(Stime=survival.time, status=survival.status, marker=eta, predict.time=30,
-#                        method="Cox", main="ROC Curve", lty=2, col="red")
-# AUC <- NULL
-# out <- CoxWeights(marker=eta, Stime=survival.time, status=survival.status, predict.time=30)
-# AUC <- out$AUC   ## to see how well the marker predicts one-month survival
-
