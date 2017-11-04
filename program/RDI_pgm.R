f2d08bddd685f7a12444c2d498dd8c7c0cae92bf
Akiko Saito <akiko.saito@nnh.go.jp>
2017-11-04 15:51:58 +0900

    modify EFS_Day

--- program/RDI_pgm.R
+++ program/RDI_pgm.R
@@ -28,18 +28,19 @@ ads6$disease_risk <- ifelse((ads6$CYTO_T821 == 1 | ads6$CYTO_INV16 == 1) & (ads6
 # ROC analysis
 survival.time <- ads6$EFS_DAY
 survival.status <- ads6$EFS_FLG
+survival.time1 <- ifelse((ads6$EFS_DAY == 0),1,(ads6$EFS_DAY))
 survival.time2 <- ads6$OS_DAY
 survival.status2 <- ads6$DETH_FLG
-marker
 
-fit0 <- coxph(Surv(survival.time2, survival.status2) 
+
+fit0 <- coxph(Surv(survival.time1, survival.status) 
               ~ ARDI+AGE2C+disease_risk, data=ads6, na.action=na.omit)
 summary(fit0)
 eta <- fit0$linear.predictor
-ROC.CC10 <- risksetROC(Stime=survival.time2, status=survival.status2, marker=eta, predict.time=1095,
+ROC.CC10 <- risksetROC(Stime=survival.time1, status=survival.status, marker=eta, predict.time=1095, 
                        method="Cox", main="ROC Curve", lty=2, col="red")
 
 AUC <- NULL
-out <- CoxWeights(marker=eta, Stime=survival.time2, status=survival.status2, predict.time=1095)
+out <- CoxWeights(marker=eta, Stime=survival.time1, status=survival.status, predict.time=1095)
                                        # to see how well the marker predicts 3-year survival
 AUC <- out$AUC
