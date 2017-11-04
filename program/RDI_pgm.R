fc4a42c5dec3cdd79abed8741921f6155ed21543
Akiko Saito <akiko.saito@nnh.go.jp>
2017-11-04 15:29:58 +0900

    Correct categorization of disease_risk. Delete ads7. Change cox covariates.

--- program/RDI_pgm.R
+++ program/RDI_pgm.R
@@ -12,30 +12,34 @@ ads3 <- merge(ads2, max_therapy, by = "ID")
 ads4 <- ads3[ads3$REAL_THERAPY_CD == ads3$MAX_T, ]   # extract rows only with max therapy code
 ads5 <- ads4[!is.na(ads4$EFS_DAY) & !is.na(ads4$EFS_FLG), ]   # extract rows w/o NA in EFS_DAY and EFS_FLG
 ads6 <- ads4[!is.na(ads4$EFF), ]   # extract rows w/o NA in EFF (efficacy analysis set)
-ads7 <- ads6[!is.na(ads6$FLT3_ITD1) & !is.na(ads6$SEX) & !is.na(ads6$AGE10) & !is.na(ads6$CYTO_FU), ]   # extract rows w/o FLT3_ITD1 and SEX and AGE1 and CYTO_FU from EFF pop. and 
 
-ads5$CYTO_T821[is.na(ads5$CYTO_T821)] <- 0
-ads5$CYTO_INV16[is.na(ads5$CYTO_INV16)] <- 0
-ads5$FLT3_ITD1[is.na(ads5$FLT3_ITD1)] <- 0
-ads5$CYTO_7[is.na(ads5$CYTO_7)] <- 0
-ads5$CYTO_5Q[is.na(ads5$CYTO_5Q)] <- 0
-ads5$CYTO_T1621[is.na(ads5$CYTO_T1621)] <- 0
-ads5$CYTO_PH1[is.na(ads5$CYTO_PH1)] <- 0
-ads5$FLT3_ITD1[is.na(ads5$FLT3_ITD1)] <- 0
+ads6$CYTO_T821[is.na(ads6$CYTO_T821)] <- 0
+ads6$CYTO_INV16[is.na(ads6$CYTO_INV16)] <- 0
+ads6$FLT3_ITD1[is.na(ads6$FLT3_ITD1)] <- 0
+ads6$CYTO_7[is.na(ads6$CYTO_7)] <- 0
+ads6$CYTO_5Q[is.na(ads6$CYTO_5Q)] <- 0
+ads6$CYTO_T1621[is.na(ads6$CYTO_T1621)] <- 0
+ads6$CYTO_PH1[is.na(ads6$CYTO_PH1)] <- 0
+ads6$FLT3_ITD1[is.na(ads6$FLT3_ITD1)] <- 0
 
-ads5$disease_risk <- ifelse((ads5$CYTO_T821 == 1 | ads5$CYTO_INV16 == 1) & (ads5$FLT3_ITD1 == 1), 1,
-                     ifelse((ads5$CYTO_7 == 1 | ads5$CYTO_5Q == 1 | ads5$CYTO_T1621 == 1 | ads5$CYTO_PH1 == 1 | ads5$FLT3_ITD1 == 1), 3, 2))
+ads6$disease_risk <- ifelse((ads6$CYTO_T821 == 1 | ads6$CYTO_INV16 == 1) & (ads6$FLT3_ITD1 == 1), 1,
+                     ifelse((ads6$CYTO_7 == 1 | ads6$CYTO_5Q == 1 | ads6$CYTO_T1621 == 1 | ads6$CYTO_PH1 == 1 | ads6$FLT3_ITD1 == 2), 3, 2))
 
 # ROC analysis
-survival.time <- ads7$EFS_DAY
-survival.status <- ads7$EFS_FLG
-survival.time2 <- ads7$OS_DAY
-survival.status2 <- ads7$DETH_FLG
+survival.time <- ads6$EFS_DAY
+survival.status <- ads6$EFS_FLG
+survival.time2 <- ads6$OS_DAY
+survival.status2 <- ads6$DETH_FLG
+marker
 
-fit0 <- coxph(Surv(survival.time2, survival.status2) ~ ARDI+FLT3_ITD1+CYTO_FU+RISK, data=ads7, na.action=na.omit)
-eta <- fit0$linear.predictors
+fit0 <- coxph(Surv(survival.time2, survival.status2) 
+              ~ ARDI+AGE2C+disease_risk, data=ads6, na.action=na.omit)
+summary(fit0)
+eta <- fit0$linear.predictor
 ROC.CC10 <- risksetROC(Stime=survival.time2, status=survival.status2, marker=eta, predict.time=1095,
                        method="Cox", main="ROC Curve", lty=2, col="red")
+
 AUC <- NULL
 out <- CoxWeights(marker=eta, Stime=survival.time2, status=survival.status2, predict.time=1095)
+                                       # to see how well the marker predicts 3-year survival
 AUC <- out$AUC
