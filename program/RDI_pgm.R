26d9f7bd8bc234521a272a2079d7557e79504825
Akiko Saito <akiko.saito@nnh.go.jp>
2017-11-04 13:39:58 +0900

    create patient population as ads6 and 7. Do OS target ROC.

--- program/RDI_pgm.R
+++ program/RDI_pgm.R
@@ -11,15 +11,19 @@ max_therapy$ID <- row.names(MAX_T)   # make ID column
 ads3 <- merge(ads2, max_therapy, by = "ID")
 ads4 <- ads3[ads3$REAL_THERAPY_CD == ads3$MAX_T, ]   # extract rows only with max therapy code
 ads5 <- ads4[!is.na(ads4$EFS_DAY) & !is.na(ads4$EFS_FLG), ]   # extract rows w/o NA in EFS_DAY and EFS_FLG
+ads6 <- ads4[!is.na(ads4$EFF), ]   # extract rows w/o NA in EFF (efficacy analysis set)
+ads7 <- ads6[!is.na(ads6$FLT3_ITD1) & !is.na(ads6$SEX) & !is.na(ads6$AGE10) & !is.na(ads6$CYTO_FU), ]   # extract rows w/o FLT3_ITD1 and SEX and AGE1 and CYTO_FU from EFF pop. and 
 
 # ROC analysis
-survival.time <- ads5$EFS_DAY
-survival.status <- ads5$EFS_FLG
+survival.time <- ads7$EFS_DAY
+survival.status <- ads7$EFS_FLG
+survival.time2 <- ads7$OS_DAY
+survival.status2 <- ads7$DETH_FLG
 
-fit0 <- coxph(Surv(EFS_DAY, EFS_FLG) ~ ARDI1, data=ads5, na.action=na.omit)
+fit0 <- coxph(Surv(survival.time2, survival.status2) ~ ARDI+FLT3_ITD1+CYTO_FU+RISK, data=ads7, na.action=na.omit)
 eta <- fit0$linear.predictors
-ROC.CC10 <- risksetROC(Stime=survival.time, status=survival.status, marker=eta, predict.time=180,
+ROC.CC10 <- risksetROC(Stime=survival.time2, status=survival.status2, marker=eta, predict.time=1095,
                        method="Cox", main="ROC Curve", lty=2, col="red")
 AUC <- NULL
-out <- CoxWeights(marker=eta, Stime=survival.time, status=survival.status, predict.time=180)
+out <- CoxWeights(marker=eta, Stime=survival.time2, status=survival.status2, predict.time=1095)
 AUC <- out$AUC
